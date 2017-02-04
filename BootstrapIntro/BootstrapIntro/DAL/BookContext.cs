e543176d487a61f4ffe42c8c02a1ab35e1a02c0c
Jeong H Park <hello@jeonghyuk.info>
2017-02-04 18:27:52 -0500

    Code First
    Completed p.61 of 277

---
+++ BootstrapIntro/BootstrapIntro/DAL/BookContext.cs
@@ -0,0 +1,30 @@
+﻿using BootstrapIntro.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+
+namespace BootstrapIntro.DAL
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        public BookContext() : base("BookContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
