8afe31c58ff754b00134189095c0cb2978390bba
Jeong H Park <hello@jeonghyuk.info>
2017-02-04 19:14:11 -0500

    Scaffold Author Model
    Completed p.75 of 277

---
+++ BootstrapIntro/BootstrapIntro/Extensions/HtmlHelperExtension.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapIntro.Extensions
+{
+    public static class HtmlHelperExtension
+    {
+        public static HtmlString HtmlConvertToJson(this HtmlHelper htmlHelper, object model)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Formatting = Formatting.Indented
+            };
+
+            var htmlStr = new HtmlString(JsonConvert.SerializeObject(model, settings));
+
+            return htmlStr;
+        }
+    }
+}
